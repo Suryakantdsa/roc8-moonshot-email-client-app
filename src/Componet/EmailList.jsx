7b3de4b00a255df6036230f13013e92717ef50a4
Suryakantdsa <dassuryakantadas@gmail.com>
2023-10-23 01:00:46 +0530

    fix some bug in filter button

--- src/Componet/EmailList.jsx
+++ src/Componet/EmailList.jsx
@@ -9,25 +9,39 @@ const EmailList = () => {
   useGetEmailFromDB();
   const allEmail = useSelector((store) => store.email);
   const [selectedEmail, setSelectedEmail] = useState(null);
+  const [selectedFilter, setSelectedFilter] = useState(null);
 
   const handleEmailClick = (email) => {
     setSelectedEmail(email);
   };
 
+  const handleFilterClick = (filter) => {
+    setSelectedFilter(filter);
+  };
+
   return (
     <div className="w-full">
-      <nav className="flex justify-start items-center w-full p-4 font-semibold">
-        <label className="mr-4">Filter by:</label>
-        <button onClick={() => setSelectedFilter("unread")} className="mr-4">
-          Unread
-        </button>
-        <button onClick={() => setSelectedFilter("read")} className="mr-4">
-          Read
-        </button>
-        <button onClick={() => setSelectedFilter("favorites")} className="mr-4">
-          Favorites
-        </button>
-      </nav>
+        <nav className="flex justify-start items-center w-full p-4 font-semibold">
+      <label className="mr-4">Filter by:</label>
+      <button
+        onClick={() => handleFilterClick("unread")}
+        className={`mr-4 px-3 py-1 rounded-full ${selectedFilter === "unread" ? 'bg-[#e1e4ea]' : ''}`}
+      >
+        Unread
+      </button>
+      <button
+        onClick={() => handleFilterClick("read")}
+        className={`mr-4 px-3 py-1 rounded-full ${selectedFilter === "read" ? 'bg-[#e1e4ea]' : ''}`}
+      >
+        Read
+      </button>
+      <button
+        onClick={() => handleFilterClick("favorites")}
+        className={`mr-4 px-3 py-1 rounded-full ${selectedFilter === "favorites" ? 'bg-[#e1e4ea]' : ''}`}
+      >
+        Favorites
+      </button>
+    </nav>
       <main className="flex ">
         <section className={` ${selectedEmail ? 'w-[200%]' : 'w-full'}`}>
           {allEmail.list ? (

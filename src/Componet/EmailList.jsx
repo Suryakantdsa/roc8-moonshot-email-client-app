c5ce1386c90cc575e938cff83b07692b2bfa9adc
Suryakantdsa <dassuryakantadas@gmail.com>
2023-10-23 03:28:28 +0530

    fix some bug in favoirate

--- src/Componet/EmailList.jsx
+++ src/Componet/EmailList.jsx
@@ -11,14 +11,15 @@ const EmailList = () => {
   useGetEmailFromDB();
   const allEmail = useSelector((store) => store.email);
   const readEmails = useSelector((state) => state.read);
-  const dispatch=useDispatch()
+  const dispatch = useDispatch();
   const favoriteEmails = useSelector((state) => state.favorites);
   const [selectedEmail, setSelectedEmail] = useState(null);
   const [selectedFilter, setSelectedFilter] = useState(null);
-
+  const [clickedFavorite, setClickedFavorite] = useState(false);
+  const [clickedRead, setClickedRead] = useState(false);
+  const [clickedUnRead, setClickedUnRead] = useState(false);
 
   useEffect(() => {
-    // Update the emails state when allEmail changes
     if (allEmail.list) {
       setEmails(allEmail.list);
     }
@@ -26,12 +27,24 @@ const EmailList = () => {
 
   const handleEmailClick = (email) => {
     setSelectedEmail(email);
-    dispatch(addRead(email))
+    dispatch(addRead(email));
   };
 
   const handleFilterClick = (filter) => {
     setSelectedFilter(filter);
+    setSelectedEmail(null);
+    setClickedFavorite(false);
+    setClickedUnRead(false);
+    setClickedRead(false);
 
+   if (filter === "read") {
+    setClickedRead(true);
+  } else if (filter === "favorites") {
+    setClickedFavorite(true);
+    setClickedRead(false);
+  } else {
+    setClickedUnRead(true);
+  }
   };
 
   return (
@@ -42,34 +55,47 @@ const EmailList = () => {
           onClick={() => handleFilterClick("unread")}
           className={`mr-4 px-3 py-1 rounded-full ${
             selectedFilter === "unread" ? "bg-[#e1e4ea]" : ""
-          }`}>
+          }`}
+        >
           Unread
         </button>
         <button
           onClick={() => handleFilterClick("read")}
           className={`mr-4 px-3 py-1 rounded-full ${
             selectedFilter === "read" ? "bg-[#e1e4ea]" : ""
-          }`}>
+          }`}
+        >
           Read
         </button>
         <button
           onClick={() => handleFilterClick("favorites")}
           className={`mr-4 px-3 py-1 rounded-full ${
             selectedFilter === "favorites" ? "bg-[#e1e4ea]" : ""
-          }`}>
+          }`}
+        >
           Favorites
         </button>
       </nav>
       <main className="flex ">
         <section className={` ${selectedEmail ? "w-[200%]" : "w-full"}`}>
-          {emails ? (
-            emails.map((data) => (
+          {clickedFavorite ? (
+            favoriteEmails?.map((data) => (
+              <a key={data.id} onClick={() => handleEmailClick(data)}>
+                <EmailCard key={data.id} email={data} />
+              </a>
+            ))
+          ) : clickedRead ? (
+            readEmails?.map((data) => (
               <a key={data.id} onClick={() => handleEmailClick(data)}>
                 <EmailCard key={data.id} email={data} />
               </a>
             ))
           ) : (
-            <Loading />
+            emails.map((data) => (
+              <a key={data.id} onClick={() => handleEmailClick(data)}>
+                <EmailCard key={data.id} email={data} />
+              </a>
+            ))
           )}
         </section>
         <section
@@ -77,7 +103,8 @@ const EmailList = () => {
             selectedEmail
               ? "translate-x-0 opacity-100"
               : "-translate-x-full opacity-0"
-          }`}>
+          }`}
+        >
           {selectedEmail && <EmailView emailDetails={selectedEmail} />}
         </section>
       </main>
